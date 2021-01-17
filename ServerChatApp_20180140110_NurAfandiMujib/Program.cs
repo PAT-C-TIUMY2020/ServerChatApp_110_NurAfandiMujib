d4086903af17c7b235c073ff2c9f8f3f01f55f4a
Nur Afandi Mujib <fndmujib@gmail.com>
2021-01-17 18:43:26 +0700

    Add project files.

---
+++ ServerChatApp_20180140110_NurAfandiMujib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_110;
+
+namespace ServerChatApp_20180140110_NurAfandiMujib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+                Console.WriteLine("Server ON, Ready to Use");
+                Console.ReadLine();
+                hostObjek.Close();
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
